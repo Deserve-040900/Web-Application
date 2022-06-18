a4d56e57522fc6307e968c79cc941513f1182342
Nguyễn Thị Hà Xuân <68209819+Deserve-040900@users.noreply.github.com>
2022-06-18 09:39:01 +0700

    using @fullcalendar/react

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import './App.css'
 import { getMonth } from './util'
+import FullCalendar from '@fullcalendar/react'
+import DayGrid from '@fullcalendar/daygrid'
 import CalendarHeader from './component/CalendarHeader'
 import Sidebar from './component/Sidebar'
 import Month from './component/Month'
@@ -11,16 +13,19 @@ function App() {
   const [ currentMonth, setCurrentMonth ] = useState(getMonth());
 
   return (
-    <React.Fragment>
-      <div className='h-screen flex flex-col'>
-        <CalendarHeader />
-        <div className='flex flex-1'>
-          <Sidebar />
-          <Month mon={currentMonth}/>
-        </div>
-      </div>
-      {/* <MyApp/> */}
-    </React.Fragment>
+    <>
+    <FullCalendar plugins={[DayGrid]} />
+    </>
+    // <React.Fragment>
+    //   <div className='h-screen flex flex-col'>
+    //     <CalendarHeader />
+    //     <div className='flex flex-1'>
+    //       <Sidebar />
+    //       <Month mon={currentMonth}/>
+    //     </div>
+    //   </div>
+    //   {/* <MyApp/> */}
+    // </React.Fragment>
   );
 }
 
